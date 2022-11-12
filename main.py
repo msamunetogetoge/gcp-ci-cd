bf4e475fa88c0f62641c599fbaba4e41913e9953
masamune <kenjiii534@gmail.com>
2022-11-12 14:12:51 +0900

    create docker file and main.py

--- main.py
+++ main.py
@@ -3,6 +3,16 @@ def add_two_number(i:int, j:int) -> int or TypeError:
         raise TypeError()
     return i+j
 
+import os
 
-if __name__== "__main__":
-    add_two_number(1,3.1)
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    name = os.environ.get("NAME", "World")
+    return "Hello {}!".format(name)
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
